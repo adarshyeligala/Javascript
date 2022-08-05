8fede8359561673ccfdb28d4a4d7491377e422ad
Adarsh <adarshyeligala09@gmail.com>
2022-08-05 11:22:10 +0530

    Reverse of any Array

--- arrayZero.ts
+++ arrayZero.ts
@@ -1,11 +1,19 @@
-function removeZeros(){
-    let a= [101, 1020, 10300, 10400, 100005, 10060];
+// function removeZeros(){
+//     let a= [101, 1020, 10300, 10400, 100005, 10060];
 
-    let output = a.map((data,index) =>{
-        return (data.toString().replace(/0/g, ""));
-    });
-    return output
-}
-  let out = removeZeros();
-  console.log(out, "res")
+//     let output = a.map((data,index) =>{
+//         return (data.toString().replace(/0/g, ""));
+//     });
+//     return output
+// }
+//   let out = removeZeros();
+//   console.log(out, "res")
   
+ function removeZeros(){
+     let a= [101, 1020, 10300, 10400, 100005, 10060].reverse();
+      console.log(a);
+     }
+   removeZeros();
+
+ 
+ 
